02c69a78030cae295f6e610e9c1fe52509ca22b4
adithya1894 <adithya1894@gmail.com>
2018-02-13 11:39:36 -0500

    added the Frontcontroller file(file in Front controller pattern)

---
+++ src/FrontController.java
@@ -0,0 +1,49 @@
+public class FrontController {
+
+    private Dispatcher dispatcher;
+
+
+    public FrontController(){
+        dispatcher = new Dispatcher();
+    }
+
+    private boolean isAuthenticUser(String view) {
+
+        Client_Controller cc_obj = new Client_Controller();
+
+        Entry ep = new Entry();
+
+
+        if (view.equalsIgnoreCase("Admin")) {
+            ep.adminLogin();
+            String id = ep.getAdminUserId();
+            String pass = ep.getAdminPass();
+            return cc_obj.loginAdmin(id, pass);
+        }else if(view.equalsIgnoreCase("User"))
+        {
+            ep.customerLogin();
+            String id = ep.getUserId();
+            String pass = ep.getPass();
+
+            return cc_obj.loginUser(id, pass);
+        }
+
+        return false;
+    }
+
+
+
+    public void dispatchRequest(String view){
+
+
+        if(isAuthenticUser(view)){
+            dispatcher.dispatch(view);
+        }
+        else {
+            dispatcher.dispatch_error("Error");
+        }
+    }
+
+
+
+}
