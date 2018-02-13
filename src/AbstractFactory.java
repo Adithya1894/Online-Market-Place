8e486e5fe586940afcf6ce712082462d90bd8112
adithya1894 <adithya1894@gmail.com>
2018-02-13 16:02:20 -0500

    added comments

--- src/AbstractFactory.java
+++ src/AbstractFactory.java
@@ -1,6 +1,21 @@
 public abstract class AbstractFactory {
 
+    /**
+     * getAdmin method of abstract factory
+     * this is used by the factory producer to get the requred object
+     * @param admin
+     * @return
+     */
     abstract Admin getAdmin(String admin);
+
+
+
+    /**
+     * getUser method of abstract factory
+     * this is used by the factory producer to get the User object
+     * @param user
+     * @return
+     */
 
     abstract User getUser(String user);
 }
