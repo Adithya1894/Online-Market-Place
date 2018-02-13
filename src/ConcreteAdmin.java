e67bd10dd86c3caad6558e5779df73575c8693d6
adithya1894 <adithya1894@gmail.com>
2018-02-13 12:52:15 -0500

    removed the while loop in concrete Admin

--- src/ConcreteAdmin.java
+++ src/ConcreteAdmin.java
@@ -6,32 +6,32 @@ public class ConcreteAdmin implements Admin {
         int i = 0;
         System.out.println("Welcome Admin");
 
-        while(i != 5){
-
-            switch (i){
-
-                case 1: {
-
-                    obj.browseItems();
-
-                    break;
-                }
-
-
-                case 2: updateItems();
-                        break;
-
-                case 3: removeItems();
-                        break;
-
-                case 4: addItems();
-                        break;
-
-                default: System.out.println("enter a valid choice");
-            }
-
-
-        }
+//        while(i != 5){
+//
+//            switch (i){
+//
+//                case 1: {
+//
+//                    obj.browseItems();
+//
+//                    break;
+//                }
+//
+//
+//                case 2: updateItems();
+//                        break;
+//
+//                case 3: removeItems();
+//                        break;
+//
+//                case 4: addItems();
+//                        break;
+//
+//                default: System.out.println("enter a valid choice");
+//            }
+//
+//
+//        }
     }
 
 
