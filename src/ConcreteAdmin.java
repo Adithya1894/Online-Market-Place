2053ba56e56f810c726e5b93619309afa7eb95e6
adithya1894 <adithya1894@gmail.com>
2018-02-13 15:30:07 -0500

    added switch statement for command pattern

--- src/ConcreteAdmin.java
+++ src/ConcreteAdmin.java
@@ -1,54 +1,82 @@
+import java.util.Scanner;
+
 public class ConcreteAdmin implements Admin {
 
-    ConcreteAdmin obj = new ConcreteAdmin();
     @Override
-    public void display() {
+    public int display() {
         int i = 0;
+        int val = 0;
+        //ConcreteAdmin obj = new ConcreteAdmin();
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Welcome Admin");
 
-//        while(i != 5){
-//
-//            switch (i){
-//
-//                case 1: {
-//
-//                    obj.browseItems();
-//
-//                    break;
-//                }
-//
-//
-//                case 2: updateItems();
-//                        break;
-//
-//                case 3: removeItems();
-//                        break;
-//
-//                case 4: addItems();
-//                        break;
-//
-//                default: System.out.println("enter a valid choice");
-//            }
-//
-//
-//        }
+        System.out.println();
+
+
+
+            System.out.println("1.browse Items");
+            System.out.println("2.Update items");
+            System.out.println("3. remove items");
+            System.out.println("4. add Items");
+
+            System.out.println("Enter Choice");
+
+            i = sc.nextInt();
+
+
+            switch (i) {
+
+
+                case 1: return 1;
+
+
+                case 2: return 2;
+
+
+                case 3: return 3;
+
+
+                case 4: return 4;
+
+
+
+            }
+
+            return 0;
+
+
+
+
     }
 
-
+    @Override
     public void browseItems(){
 
+        System.out.println("Browse Items will be updated next time");
+
 
     }
-
+    @Override
     public void updateItems(){
 
-    }
 
+        System.out.println("Update Items will be updated next time");
+
+    }
+    @Override
     public void removeItems(){
 
-    }
 
+        System.out.println("Remove Items will be updated next time");
+
+    }
+    @Override
     public void addItems(){
+
+
+        System.out.println("Add Items will be updated next time");
 
     }
 
