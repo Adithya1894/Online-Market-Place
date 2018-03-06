856f05bb4913d407e4f15e2dc4587e1b2c24a990
adithya1894 <adithya1894@gmail.com>
2018-03-06 18:52:31 -0500

    added comments

--- src/RequiresRole.java
+++ src/RequiresRole.java
@@ -10,11 +10,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+/**
+ * Custom annotaiton for implementing Role based access control
+ * The retentionPolicy.RUNTIME determines that this policy is preserved till runtime
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequiresRole {
     String value();
-
 
 }
