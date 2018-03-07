dc04b0f404c0d3e0c9d7217936a618440c97e06e
adithya1894 <adithya1894@gmail.com>
2018-03-06 21:34:35 -0500

    added the additional functionality for User

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -9,25 +9,24 @@
 public class Dispatcher {
 
 
-
     private AbstractFactory adminFactory;
 
     private AbstractFactory userFactory;
 
 
-    public Dispatcher(){
+    public Dispatcher() {
         //trying to access the concrete classes via FactoryProducer
         adminFactory = FactoryProducer.getFactory("Admin");
 
         userFactory = FactoryProducer.getFactory("User");
 
     }
+
     //added the session variable
-    public void dispatch(String view, Session session){
+    public void dispatch(String view, Session session) {
 
         //gives the object of concrete Admin
-        if(view.equalsIgnoreCase("Admin"))
-        {
+        if (view.equalsIgnoreCase("Admin")) {
             //calling the display method of the concreteAdmin class
             Admin admin = adminFactory.getAdmin("Admin");
             int option = admin.display();
@@ -36,7 +35,7 @@ public class Dispatcher {
             //object of concreteAdmin will be created here
             ConcreteBrowseItems cbr = new ConcreteBrowseItems(admin, session);
 
-            ConcreteUpdateItems cui = new ConcreteUpdateItems(admin,session);
+            ConcreteUpdateItems cui = new ConcreteUpdateItems(admin, session);
 
             ConcreteRemoveItems cri = new ConcreteRemoveItems(admin, session);
 
@@ -46,12 +45,11 @@ public class Dispatcher {
             ConcretePurchase concretePurchase = new ConcretePurchase(admin, session);
 
 
-
             //invoker object to invoke the functionality of command pattern
-            InvokerAdmin ia = new InvokerAdmin();
+            Invoker ia = new Invoker();
 
 
-            switch(option){
+            switch (option) {
 
                 case 1: {
                     //we are invoking the command and executing them
@@ -66,7 +64,7 @@ public class Dispatcher {
                     break;
                 }
 
-                case 3:{
+                case 3: {
                     ia.takeCommand(cri);
                     ia.placeCommand();
                     break;
@@ -83,8 +81,7 @@ public class Dispatcher {
                     ia.takeCommand(concretePurchase);
                     ia.placeCommand();
                 }
-        }
-
+            }
 
 
         }
@@ -92,19 +89,47 @@ public class Dispatcher {
         else {
 
             User user = userFactory.getUser("User");
-            user.display();
+
+            int option = user.display();
+
+
+            ConcreteUserBrowseItems cbr = new ConcreteUserBrowseItems(user, session);
+
+            ConcreteUserPurchaseItems concreteUserPurchaseItems = new ConcreteUserPurchaseItems(user, session);
+
+
+            Invoker ia = new Invoker();
+
+
+            switch (option) {
+
+                case 1: {
+                    //we are invoking the command and executing them
+                    ia.takeCommand(cbr);
+                    ia.placeCommand();
+                    break;
+                }
+
+                case 2: {
+                    ia.takeCommand(concreteUserPurchaseItems);
+                    ia.placeCommand();
+                    break;
+                }
+
+
+            }
 
         }
-
     }
-    //if the authentication fails in frontController then the error page is displayed
-    public void dispatch_error(String view){
-        if(view.equalsIgnoreCase("ERROR")){
-            ErrorPage obj = new ErrorPage();
-            obj.print();
+        //if the authentication fails in frontController then the error page is displayed
+        public void dispatch_error(String view){
+            if(view.equalsIgnoreCase("ERROR")){
+                ErrorPage obj = new ErrorPage();
+                obj.print();
 
+            }
         }
     }
 
 
-}
+
