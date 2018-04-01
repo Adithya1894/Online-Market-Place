f7b9ccbe8ab922b22ff3f029f71058fa847384ba
adithya1894 <adithya1894@gmail.com>
2018-04-01 18:48:03 -0400

    changed the formatting

--- src/ConcreteUser.java
+++ src/ConcreteUser.java
@@ -13,6 +13,13 @@ import java.util.Scanner;
 //concrete user class of the Abstractfactory pattern, object of this class is created based upon the logic
 public class ConcreteUser implements User {
 
+    Client_Controller client_controller;
+    public ConcreteUser() {
+
+         client_controller = new Client_Controller();
+
+    }
+
     /**
      *
      */
@@ -30,6 +37,7 @@ public class ConcreteUser implements User {
         System.out.println("Welcome Customer");
 
         System.out.println();
+
 
 
             System.out.println("1.browse Items");
@@ -51,14 +59,13 @@ public class ConcreteUser implements User {
                 case 2:
                     return 2;
 
-                default:System.out.println("Please enter a valid choice! from the menu, you entered" +i);
+                default:
+                    System.out.println("Please enter a valid choice! from the menu, you entered" + i);
             }
-
 
 
         return 0;
     }
-
 
     /**
      *
@@ -67,7 +74,7 @@ public class ConcreteUser implements User {
     @Override
     public void browseItems(Session session) {
 
-        Client_Controller client_controller = new Client_Controller();
+
 
         List<String> val;
 
@@ -86,7 +93,7 @@ public class ConcreteUser implements User {
 
 
             //Formatting the output.
-            System.out.printf("\n%s %-7s %-10s %-15s %-30s\n",itemId,itemPrice,itemStock,itemName,itemDescription);
+            System.out.printf("\n%-7s %-15s %-10s %-15s %-30s\n",itemId,itemName,itemStock,itemPrice,itemDescription);
             for(int i=0;i<val.size();i++)
             {
                 item=val.get(i).split(",");
@@ -94,10 +101,10 @@ public class ConcreteUser implements User {
                 {
                     if(j==0){
 
-                        System.out.printf("%s",item[j]);
+                        System.out.printf("%-7s",item[j]);
                     }
                     else if(j==1){
-                        System.out.printf("%-7s",item[j]);
+                        System.out.printf("%-15s",item[j]);
                     }
                     else if(j==2){
                         System.out.printf("%-10s",item[j]);
@@ -125,17 +132,17 @@ public class ConcreteUser implements User {
     @Override
     public void purchase(Session session) {
 
-        Client_Controller client_controller = new Client_Controller();
 
         //method returns a string of array.
         try {
-            client_controller.purchaseItems(session, 3);
-            System.out.println("Method not implemented yet, but you are granted access to this method");
+            if(client_controller.purchaseItems(session, 3))
+                System.out.println("Item purchase success!");
+
+            else
+                System.out.println("Item out of stock");
         }
         catch (AuthorizationException e){
             System.out.println(e.getMessage());
         }
     }
-
-
 }
