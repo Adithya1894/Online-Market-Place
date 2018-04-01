0ef3a17c18a17012b71cc94ccb99649cc245eb3e
adithya1894 <adithya1894@gmail.com>
2018-04-01 18:46:47 -0400

    added the complete functionality

--- src/ServerController.java
+++ src/ServerController.java
@@ -74,7 +74,7 @@ public class ServerController extends UnicastRemoteObject implements MPlaceInter
     }
 
     @Override
-    public boolean addItems(Session session, String[] items) throws RemoteException{
+    public synchronized boolean addItems(Session session, String[] items) throws RemoteException{
 
         //boolean variable to determine the status of the add item query.
         boolean status;
@@ -99,7 +99,7 @@ public class ServerController extends UnicastRemoteObject implements MPlaceInter
 
     //function which displays all the available products to the user.
     @Override
-    public List<String> browsingUser(Session session) throws RemoteException {
+    public synchronized List<String> browsingUser(Session session) throws RemoteException {
 
 
         //Creating a new arrayList to add the items and return it to the Client
@@ -118,7 +118,7 @@ public class ServerController extends UnicastRemoteObject implements MPlaceInter
                 while(resultSet.next())
                 {
                     //getting the result from the database using the DbConnection class
-                    data=resultSet.getString("item_id")+","+resultSet.getString("item_price")+","+resultSet.getString("item_stock")+","+resultSet.getString("item_name")+","+resultSet.getString("item_description");
+                    data=resultSet.getString("item_id")+","+resultSet.getString("item_name")+","+resultSet.getString("item_stock")+","+resultSet.getString("item_price")+","+resultSet.getString("item_description");
                     //adding the items into the arrayList
                     itemList.add(i,data);
                     i++;
@@ -149,8 +149,54 @@ public class ServerController extends UnicastRemoteObject implements MPlaceInter
     }
 
     @Override
-    public void purchase(Session session, int itemId) throws RemoteException {
+    public synchronized boolean purchase(Session session, int itemId) throws RemoteException {
 
+
+        ResultSet resultSet = null;
+        int stock = 0;
+
+        if(dbConnection.isConnectionEstablished()){
+            try{
+                resultSet = dbConnection.getUniqueItem(itemId);
+                while(resultSet.next())
+                {
+                    stock=Integer.parseInt(resultSet.getString("item_stock").toString());
+
+                    System.out.println(stock);
+                }
+                resultSet.beforeFirst();
+
+
+                while(resultSet.next())
+                {
+                    if(!(stock<=0)){
+                        //reduce the stock by 1 after purchase
+
+                        System.out.println("before substraction"+stock);
+
+                        stock=stock-1;
+
+                        System.out.println(stock);
+                        //calling the database
+                        if(dbConnection.purchaseItems(itemId, stock));
+                        {
+                            //System.out.println(val);
+                            //if success returns true
+                            return true;
+                        }
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                System.out.println("Hello bug");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
     }
 
     @Override
@@ -182,7 +228,7 @@ public class ServerController extends UnicastRemoteObject implements MPlaceInter
             //Naming our server so that it can be binded to the registry
             String name = "//10.234.136.55:1895/server";
 
-
+            //creating the proxy
             MPlaceInterface stub = (MPlaceInterface) Proxy.newProxyInstance(MPlaceInterface.class.getClassLoader(),
                     new Class<?>[] {MPlaceInterface.class},
                     new AuthorizationInvocationHandler(new ServerController()));
