9c45a0da0aa15f11d90d4f2688816d9c1b36f175
adithya1894 <adithya1894@gmail.com>
2018-04-01 23:08:07 -0400

    updated the file

--- src/ConcreteUser.java
+++ src/ConcreteUser.java
@@ -96,6 +96,7 @@ public class ConcreteUser implements User {
             System.out.printf("\n%-7s %-15s %-10s %-15s %-30s\n",itemId,itemName,itemStock,itemPrice,itemDescription);
             for(int i=0;i<val.size();i++)
             {
+                //displaying the formatted output.
                 item=val.get(i).split(",");
                 for(int j=0;j<item.length;j++)
                 {
@@ -110,7 +111,7 @@ public class ConcreteUser implements User {
                         System.out.printf("%-10s",item[j]);
                     }
                     else if(j==3){
-                        System.out.printf("%-15s"+"$",item[j]);
+                        System.out.printf("$"+"%-15s",item[j]);
                     }else{
                         System.out.printf("%-30s", item[j]);
                     }
@@ -132,15 +133,20 @@ public class ConcreteUser implements User {
     @Override
     public void purchase(Session session) {
 
-
+        System.out.println("Enter the itemId: ");
+        //creating a scanner object
+        Scanner scanner = new Scanner(System.in);
+        //taking the item id input from the user.
+        int option = scanner.nextInt();
         //method returns a string of array.
         try {
-            if(client_controller.purchaseItems(session, 3))
+            if(client_controller.purchaseItems(session, option))
                 System.out.println("Item purchase success!");
 
             else
                 System.out.println("Item out of stock");
         }
+        //throws an authorization exception if the user is not authorized to access this method.
         catch (AuthorizationException e){
             System.out.println(e.getMessage());
         }
