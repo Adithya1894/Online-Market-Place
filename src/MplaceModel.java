ab71d5e0008b9ef7cb4ba096816c2ca70d90b847
adithya1894 <adithya1894@gmail.com>
2018-04-02 19:52:51 -0400

    added the implementation of add items, browse, purchase methods

--- src/MplaceModel.java
+++ src/MplaceModel.java
@@ -8,9 +8,14 @@
 //amorampu
 
 import java.rmi.RemoteException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MplaceModel {
 
-	// Ryan: Please include useful comments in each file.
+    // Ryan: Please include useful comments in each file.
     //fixed
     //private fields so, other classes cannot access these fields
     private String username;
@@ -18,7 +23,13 @@ public class MplaceModel {
     private String adminId;
     private String adminPass;
 
-    public MplaceModel() throws RemoteException{
+
+    DbConnection dbConnection;
+
+    ResultSet resultSet = null;
+
+
+    public MplaceModel() throws RemoteException {
 
         //initializing the variables
         username = "amorampu";
@@ -26,33 +37,165 @@ public class MplaceModel {
         adminId = "admin";
         adminPass = "12345";
 
+        //creating the object of the DbConnection class.
+        dbConnection = new DbConnection();
+        //setting the connection
+        dbConnection.setConnectionEstablished();
+        //name = m;
+
 
     }
 
-     boolean loginUser(String uname, String pass) {
+    boolean loginUser(String uname, String pass) {
 
         //making a comparison to check if the entered username equals the initialized one
-         if (this.username.equals(uname) && this.userpass.equals(pass))
+        if (this.username.equals(uname) && this.userpass.equals(pass))
 
-             return true;
+            return true;
 
-         else {
-             return false;
-         }
-     }
+        else {
+            return false;
+        }
+    }
 
-     //Athentication for admin
-     boolean loginAdmin(String adminId, String adminPass) {
-         //System.out.println(adminId +" "+adminPass);
-         if (this.adminId.equals(adminId) && this.adminPass.equals(adminPass))
-         {
+    //Athentication for admin
+    boolean loginAdmin(String adminId, String adminPass) {
+        //System.out.println(adminId +" "+adminPass);
+        if (this.adminId.equals(adminId) && this.adminPass.equals(adminPass)) {
 
-             return true;
-         }
-         else {
-             return false;
-         }
-     }
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    /**
+     * purchase method to purchase the items and change the stock to stock - 1 in the database.
+     *
+     * @param session
+     * @param itemId
+     * @return
+     */
+    public boolean purchase(Session session, int itemId) {
+
+        ResultSet resultSet = null;
+        int stock = 0;
+
+        //checking if the conection is established
+        if (dbConnection.isConnectionEstablished()) {
+            try {
+                //getting the resultset of the required item to find the available stock
+                resultSet = dbConnection.getUniqueItem(itemId);
+                while (resultSet.next()) {
+                    //getting  the stock and storing it in the stock variable
+                    stock = Integer.parseInt(resultSet.getString("item_stock").toString());
+
+                    System.out.println(stock);
+                }
+                //chaging the result set to previous point as, it is null now
+                resultSet.beforeFirst();
+
+
+                while (resultSet.next()) {
+                    if (!(stock <= 0)) {
+                        //reduce the stock by 1 after purchase
+
+                        System.out.println("before substraction" + stock);
+                        //changing the stock value to stock - 1, so we can update the table
+                        stock = stock - 1;
+
+                        System.out.println(stock);
+                        //calling the database
+                        if (dbConnection.purchaseItems(itemId, stock)) ;
+                        {
+                            //System.out.println(val);
+                            //if success returns true
+                            return true;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                System.out.println("Hello bug");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
+
+
+    }
+
+    /**
+     *
+     * @param session
+     * @return
+     */
+    public List<String> browsingUser(Session session) {
+
+        //Creating a new arrayList to add the items and return it to the Client
+        List<String> itemList = new ArrayList<>();
+        String data;
+        int i = 0;
+
+        //careating a new object of the DbConnection class
+
+        //checking if the connection is established
+        if (dbConnection.isConnectionEstablished()) {
+
+            try {
+
+                resultSet = dbConnection.getResultSet();
+                while (resultSet.next()) {
+                    //getting the result from the database using the DbConnection class
+                    data = resultSet.getString("item_id") + "," + resultSet.getString("item_name") + "," + resultSet.getString("item_stock") + "," + resultSet.getString("item_price") + "," + resultSet.getString("item_description");
+                    //adding the items into the arrayList
+                    itemList.add(i, data);
+                    i++;
+                }
+
+                return itemList;
+
+                //catching the sql exception
+            } catch (SQLException e) {
+
+                System.out.println("cannot get the result!");
+
+            }
+
+        }
+
+        return null;
+    }
+
+    /**
+     *Add items method to add the items to the database, this is only accessible to admin
+     * @param items
+     * @return
+     */
+    public boolean addItems(String[] items) {
+
+
+        //boolean variable to determine the status of the add item query.
+        boolean status;
+
+        if (items != null) {
+
+            //calling the addItems method to add the item into the database.
+            status = dbConnection.addItems(items);
+
+            if (status) {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        return false;
+
+    }
 
 }
