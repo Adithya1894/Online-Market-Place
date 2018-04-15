0ccde2f225257f181737201a7826cc4d9ba0193c
adithya1894 <adithya1894@gmail.com>
2018-04-15 19:46:33 -0400

    added newUser factory into abstract factory

--- src/FactoryProducer.java
+++ src/FactoryProducer.java
@@ -13,11 +13,14 @@
 public class FactoryProducer {
 
     public static AbstractFactory getFactory(String option){
-        //if the user wants a Admin view, then this will return the AdminFactory, if not it will return the UserFactory.
+        //if the user wants a Admin view, then this will return the AdminFactory, if not it will return the UserFactory, else it will return newUserFactory.
         if(option.equalsIgnoreCase("Admin")){
             return new AdminFactory();
         }else if(option.equalsIgnoreCase("User"))
             return new UserFactory();
+        else if(option.equalsIgnoreCase("newUser")){
+            return new NewUserFactory();
+        }
 
         return null;
     }
