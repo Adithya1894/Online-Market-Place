1ceea59d1672257d8d80b5a4c19992100e47f6a6
adithya1894 <adithya1894@gmail.com>
2018-04-15 17:59:25 -0400

    added the new user registered welcome page

---
+++ src/NewUserPage.java
@@ -0,0 +1,15 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given nor
+// received any help on this assignment.
+//
+
+//amorampu
+
+
+public class NewUserPage {
+    //Method to print the new User registered message
+    public void print(String name){
+        System.out.println("Welcome: "+name+"Now you can use the services");
+    }
+}
